ead383f18c98528bdab01bd33d7586a09212c454
georgemargiolos <31922827+georgemargiolos@users.noreply.github.com>
2025-12-26 03:41:07 -0500

    v88.0: Fix mobile card width - Reduced column padding from 12px to 5px to match Our Fleet page

--- yolo-yacht-search/yolo-yacht-search.php
+++ yolo-yacht-search/yolo-yacht-search.php
@@ -3,7 +3,7 @@
  * Plugin Name: YOLO Yacht Search & Booking
  * Plugin URI: https://github.com/georgemargiolos/LocalWP
  * Description: Yacht search plugin with Booking Manager API integration for YOLO Charters. Features search widget and results blocks with company prioritization.
- * Version: 87.3
+ * Version: 88.0
  * Author: George Margiolos
  * Author URI: https://github.com/georgemargiolos
  * License: GPL v2 or later
@@ -20,7 +20,7 @@ if (!defined('WPINC')) {
 }
 
 // Plugin version
-define('YOLO_YS_VERSION', '87.9');
+define('YOLO_YS_VERSION', '88.0');
 
 // Plugin directory path
 define('YOLO_YS_PLUGIN_DIR', plugin_dir_path(__FILE__));
